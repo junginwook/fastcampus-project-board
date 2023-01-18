30b69472fffc3211b80a89b235bedcab1b33dc56
inwookjung <inwook.jung@kr.doubledown.com>
2023-01-19 00:38:29 +0900

    #1 - querydsl 지원을 이용하여 api 검색 옵션 추가
    QuerydslPredicateExecutor
    QuerydslBinderCustomizer 를 이용하여 간단하게 개발

--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -1,10 +1,34 @@
 package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+		JpaRepository<Article, Long>,
+		//entityd에 대한 기본 검색기능을 추가해준다
+		//부분 검색은 지원하고 있지 ㅇ낳음
+		QuerydslPredicateExecutor<Article>,
+		//입맛에 맞는 검색 기능을 추가하기 위해서
+		QuerydslBinderCustomizer<QArticle> {
 
+	@Override
+	default void customize(QuerydslBindings bindings, QArticle root) {
+		//모든 필드들에 대한 검색이 열러 있는데
+		bindings.excludeUnlistedProperties(true);
+		bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+		bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // '%{v}%'
+		bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like
+		bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // like
+		bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+		bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+	}
 }
