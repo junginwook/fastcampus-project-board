973cbe943308591312ef52024f5f30f727b2425c
inwookjung <inwook.jung@kr.doubledown.com>
2023-01-19 23:38:33 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤러 핸들러 메소드로  테스트를 통과하게 열어주고
    뷰 적절히 뼈대 잡은 
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 때문에 적절히 테스트 메소드 수정

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO board.article
+(created_at, created_by, modified_at, modified_by, content, hashtag, title)
+VALUES('2020-01-02', 'aadf', '2020-01-02', 'aaaaa', 'ddfff', 'dfdfd', 'dfdfdf');
--- src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectboard/controller/ArticleControllerTest.java
@@ -26,7 +26,6 @@ class ArticleControllerTest {
 		this.mvc = mvc;
 	}
 
-	@Disabled
 	@DisplayName("[view][GET] 게시글 리스트 (게시판) 페이지 - 정상 호출")
 	@Test
 	public void givenNothing_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception {
@@ -35,7 +34,7 @@ class ArticleControllerTest {
 		//when & then
 		mvc.perform(get("/articles"))
 				.andExpect(status().isOk())
-				.andExpect(content().contentType(MediaType.TEXT_HTML))
+				.andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
 				.andExpect(view().name("articles/index"))
 				//modelAttribute 확인
 				.andExpect(model().attributeExists("articles"));
