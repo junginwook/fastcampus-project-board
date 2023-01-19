52089db407d3340a8d3494cd42943907733f7222
inwookjung <inwook.jung@kr.doubledown.com>
2023-01-20 01:19:52 +0900

    #16 - 게시글 뷰 기본 골격 작성
    컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/test/java/com/fastcampus/projectboard/repository/ThreadPoolExecutorTest.java
+++ src/test/java/com/fastcampus/projectboard/repository/ThreadPoolExecutorTest.java
@@ -12,32 +12,32 @@ public class ThreadPoolExecutorTest {
 	@Test
 	void threadPoolExecutorTest() throws InterruptedException {
 
-		int numTasks = 60;
-		BlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
-		CountDownLatch countDownLatch = new CountDownLatch(numTasks);
-
-		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10, 50, 10,
-				TimeUnit.SECONDS, blockingQueue);
-
-		for(int i = 0; i<120; i++) {
-			threadPoolExecutor.submit(() -> {
-				try {
-					Thread.sleep(1000);
-				} catch (Exception e) {
-
-				}
-				countDownLatch.countDown();
-			});
-		}
-
-		for(int i=0; i< 120; i++) {
-			Thread.sleep(500);
-
-			System.out.println("Active: " + threadPoolExecutor.getActiveCount());
-
-			System.out.println("Queue: " + blockingQueue.size());
-		}
-
-		threadPoolExecutor.shutdown();
+//		int numTasks = 60;
+//		BlockingQueue<Runnable> blockingQueue = new LinkedBlockingQueue<>();
+//		CountDownLatch countDownLatch = new CountDownLatch(numTasks);
+//
+//		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(10, 50, 10,
+//				TimeUnit.SECONDS, blockingQueue);
+//
+//		for(int i = 0; i<120; i++) {
+//			threadPoolExecutor.submit(() -> {
+//				try {
+//					Thread.sleep(1000);
+//				} catch (Exception e) {
+//
+//				}
+//				countDownLatch.countDown();
+//			});
+//		}
+//
+//		for(int i=0; i< 120; i++) {
+//			Thread.sleep(500);
+//
+//			System.out.println("Active: " + threadPoolExecutor.getActiveCount());
+//
+//			System.out.println("Queue: " + blockingQueue.size());
+//		}
+//
+//		threadPoolExecutor.shutdown();
 	}
 }
